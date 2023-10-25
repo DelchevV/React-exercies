289c65dd5096c31bb5919d680a42fb41a56a76cf
Veselin Delchev <delchev.v5454@gmail.com>
2023-10-25 14:13:57 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Text } from './Text'
 
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [showText, setShowText] = useState(false);
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value)
-  }
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false
-    }
-    setTodoList([...todoList, task])
-  };
-
-  const deleteAction = (taskName) => {
-    const newTodoList = todoList.filter((task) => {
-      return task === taskName ? false : true;
-    })
-
-    setTodoList(newTodoList)
-  }
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task)=>{
-        if (task.id === id ){
-          return {...task, completed: true}
-        }else{
-          return task
-        }
-      })
-    )
-    
-  }
+   
 
   return (
     <div className="App">
-      <div className='addTask'>
-        <input onChange={handleChange}></input>
-        <button onClick={addTask}>Add Task</button>
-      </div>
+      <button onClick={()=>{
+        setShowText(!showText);
+      }}>
+        Show Text
+      </button>
 
-      <div className='list'>
-        {todoList.map((task) => {
-          return (
-            <div className='task' style={ {backgroundColor: task.completed ? 'green' : 'white'}}>
-              <h2>{task.taskName}</h2>
-              <button onClick={() => completeTask(task.id)}>Complete</button>
-              <button onClick={() => deleteAction(task)}>X</button>
-            </div>
-          )
-        })}
-
-      </div>
-
+      {showText && <Text/>}
     </div>
   );
 }
--- src/User.js
+++
@@ -1,9 +0,0 @@
-
-
-export const Player = (props) => {
-    return (
-        <div>
-            <h1>{props.name} is {props.age} years old.</h1>
-        </div>
-    );
-};
