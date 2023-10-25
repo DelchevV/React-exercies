770e05f41ef6520f8cdbf19251f6733c6d0c487b
Veselin Delchev <delchev.v5454@gmail.com>
2023-10-25 15:31:32 +0300

    Excuse Generator

--- src/App.js
+++ src/App.js
@@ -5,22 +5,43 @@ import Axios from 'axios'
 
 
 function App() {
-  const [fact, setFact] = useState('')
+  const [excuse, setExcuse]=useState('')
+  
 
-  const serveFact = ()=>{
-    Axios.get('https://catfact.ninja/fact').then((res)=>{
-      setFact(res.data.fact)
+  const fetchFamilyExcuse= ()=>{
+    Axios.get('https://excuser-three.vercel.app/v1/excuse/family/').then((response)=>{
+      setExcuse(response.data[0].excuse)
     })
   }
 
-  useEffect(()=>{
-    serveFact();
-  },[])
+  const fetchPartyExcuse= ()=>{
+    Axios.get('https://excuser-three.vercel.app/v1/excuse/party/').then((response)=>{
+      setExcuse(response.data[0].excuse)
+    })
+  }
+
+  const fetchOfficeExcuse= ()=>{
+    Axios.get('https://excuser-three.vercel.app/v1/excuse/office/').then((response)=>{
+      setExcuse(response.data[0].excuse)
+    })
+  }
+
+  const fetchGamingExcuse= ()=>{
+    Axios.get('https://excuser-three.vercel.app/v1/excuse/gaming/').then((response)=>{
+      setExcuse(response.data[0].excuse)
+    })
+  }
+ 
 
   return (
     <div className="App">
-      <button onClick={serveFact}>Generate Cat Fact</button>
-      <p>{fact}</p>
+      <h1>Excuses Generator</h1>
+      <button onClick={fetchFamilyExcuse}>Family</button>
+      <button onClick={fetchPartyExcuse}>Party</button>
+      <button onClick={fetchOfficeExcuse}>Office</button>
+      <button onClick={fetchGamingExcuse}>Gaming</button>
+
+      <h1>{excuse}</h1>
     </div>
   );
 }
