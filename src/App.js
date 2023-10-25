98c3f40e934f3c957889e0cff45d675b810f9cb5
Veselin Delchev <delchev.v5454@gmail.com>
2023-10-25 12:46:19 +0300

    Making ToDo List with CRUD

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-
 import './App.css';
 import { useState } from 'react';
 
-import { Player } from './User'
 
 function App() {
-  const [showText, setShowText] = useState(true);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-  const hideOrShow = (e) => {
-    setShowText(showText ? false : true)
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false
+    }
+    setTodoList([...todoList, task])
   };
+
+  const deleteAction = (taskName) => {
+    const newTodoList = todoList.filter((task) => {
+      return task === taskName ? false : true;
+    })
+
+    setTodoList(newTodoList)
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task)=>{
+        if (task.id === id ){
+          return {...task, completed: true}
+        }else{
+          return task
+        }
+      })
+    )
+    
+  }
+
   return (
-
     <div className="App">
-      <button onClick={hideOrShow}>Show/Hide</button>
-      {showText === true && <h1>Hello there!</h1>}
-    </div>
+      <div className='addTask'>
+        <input onChange={handleChange}></input>
+        <button onClick={addTask}>Add Task</button>
+      </div>
 
+      <div className='list'>
+        {todoList.map((task) => {
+          return (
+            <div className='task' style={ {backgroundColor: task.completed ? 'green' : 'white'}}>
+              <h2>{task.taskName}</h2>
+              <button onClick={() => completeTask(task.id)}>Complete</button>
+              <button onClick={() => deleteAction(task)}>X</button>
+            </div>
+          )
+        })}
+
+      </div>
+
+    </div>
   );
 }
 
