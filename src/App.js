91c8f5c11458cbc34f6f42b03e5ed13ff3fa7d2e
Veselin Delchev <delchev.v5454@gmail.com>
2023-10-26 13:29:38 +0300

    React Forms
    libraries:
    yup
    react-hook-form
    @hookform-resolver

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
+
 import './App.css';
-import { useState, useEffect, createContext } from 'react';
-import Axios from 'axios'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Contact } from './pages/Contact'
-import { Menu } from './pages/Menu'
-import { Error404 } from './pages/Error404'
-import { Profile } from './Profile'
-import { Navbar } from './Navbar'
+import {Form} from './components/From'
 
-
-export const AppContext = createContext();
 
 function App() {
-  const [username, setUsername] = useState('Veselin');
+  
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/menu' element={<Menu />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='*' element={<Error404 />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <Form/>
     </div>
   );
 }
--- src/pages/Error404.js
+++
@@ -1,3 +0,0 @@
-export const Error404 = ()=>{
-    return <h1>404. This page wasn't found!</h1>
-}
